d466584582ea07186c79e338088a2f36bdab1d54
Maxsuel Marinho <maxsuelmarinho@gmail.com>
2019-07-09 22:08:50 -0300

    Securing the backend with Auth0

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,11 +4,39 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+
 // define the Express app
 const app = express();
 
 // the database
 const questions = [];
+
+const APP_PORT = process.env.APP_PORT || 8080;
+const checkJwtMock = function(req, res, next) {
+    console.log('Check JWT Mock');
+    req.user = {
+        name: 'User Mock',
+    };
+    next();
+};
+
+const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN;
+const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID;
+const checkJwt = AUTH0_DOMAIN && AUTH0_CLIENT_ID ? jwt({
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://${AUTH0_DOMAIN}/.well-known/jwks.json`
+    }),
+
+    // validate the audience and the issuer.
+    audience: `${AUTH0_CLIENT_ID}`,
+    issuer: `https://${AUTH0_DOMAIN}/`,
+    algorithms: ['RS256']
+}) : checkJwtMock;
 
 // enhance your app security with Helmet
 app.use(helmet());
@@ -43,7 +71,7 @@ app.get('/:id', (req, res) => {
 });
 
 // insert a new question
-app.post('/', (req, res) => {
+app.post('/', checkJwt, (req, res) => {
     const {title, description} = req.body;
     const newQuestion = {
         id: questions.length + 1,
@@ -57,7 +85,7 @@ app.post('/', (req, res) => {
 })
 
 // insert a new answer to a question
-app.post('/answer/:id', (req, res) => {
+app.post('/answer/:id', checkJwt, (req, res) => {
     const {answer} = req.body;
 
     const question = questions.filter(q => (q.id === parseInt(req.params.id)));
@@ -66,12 +94,13 @@ app.post('/answer/:id', (req, res) => {
 
     question[0].answers.push({
         answer,
+        author: req.user.name,
     });
 
     res.status(201).send();
 });
 
 // start the server
-app.listen(8080, () => {
-    console.log('listening on port 8080');
+app.listen(APP_PORT, () => {
+    console.log(`listening on port ${APP_PORT}`);
 });
